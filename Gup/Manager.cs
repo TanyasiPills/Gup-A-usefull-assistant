58cf6b113083e0f3e5974fe33be6cdeb259f927f
TanyasiPills <matyasg760@gmail.com>
2025-09-15 15:46:38 +0200

    working transparent window in opengl

---
+++ Gup/Manager.cs
@@ -0,0 +1,35 @@
+﻿using OpenTK.Graphics.OpenGL;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+
+namespace Gup
+{
+    internal unsafe class Manager
+    {
+        public static Window* Initialization()
+        {
+            Console.WriteLine("Gup Initialized");
+            GLFW.Init();
+            VideoMode* mode = GLFW.GetVideoMode(GLFW.GetPrimaryMonitor());
+            int screenWidth = mode->Width;
+            int screenHeight = mode->Height;
+
+            GLFW.WindowHint(WindowHintBool.Decorated, true);
+            GLFW.WindowHint(WindowHintBool.Resizable, true);
+            GLFW.WindowHint(WindowHintBool.TransparentFramebuffer, true);
+
+
+            Window* window = GLFW.CreateWindow(screenWidth, screenHeight, "Gup Window", null, null);
+
+            GLFW.MakeContextCurrent(window);
+            GL.LoadBindings(new GLFWBindingsContext());
+
+            GL.Enable(EnableCap.Blend);
+            GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
+
+            //GLFW.MaximizeWindow(window);
+
+            return window;
+        }
+    }
+}
---
+++ Gup/Program.cs
@@ -0,0 +1,31 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+
+namespace Gup
+{
+
+    internal unsafe class Gup
+    {
+        static void Main(string[] args)
+        {
+            Window* window = Manager.Initialization();
+
+            while (true)
+            {
+                if (GLFW.WindowShouldClose(window) == true)
+                    break;
+
+                GLFW.PollEvents();
+
+                GL.ClearColor(0f, 0f, 0f, 0f);
+                GL.Clear(ClearBufferMask.ColorBufferBit);
+                GLFW.SwapBuffers(window);
+            }
+            GLFW.DestroyWindow(window);
+            GLFW.Terminate();
+        }
+    }
+
+}
